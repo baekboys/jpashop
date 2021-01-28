07134ca5fc72a17ed4513a41b7ec907b2abb1294
Myungsun Baek <baekboys@naver.com>
2021-01-29 02:21:11 +0900

    jpa 환경 설정 추가

---
+++ src/test/java/jpabook/jpashop/MemberRpositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRpositoryTest {
+
+    @Autowired
+    MemberRpository memberRpository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long saveId = memberRpository.save(member);
+
+        Member findMember = memberRpository.find(saveId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
