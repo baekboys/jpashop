a30234bce96333a58285b89b1f5f4ce998db0cfe
Myungsun Baek <baekboys@naver.com>
2021-01-30 01:14:25 +0900

    엔티티 클래스 작성

--- src/test/java/jpabook/jpashop/MemberRpositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRpositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,13 +22,13 @@ public class MemberRpositoryTest {
     @Rollback(value = false)
     public void testMember() throws Exception {
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
         Long saveId = memberRpository.save(member);
 
         Member findMember = memberRpository.find(saveId);
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
