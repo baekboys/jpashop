6ac9e891110c3a2d76dde294214c5afb70ecac0c
Myungsun Baek <baekboys@naver.com>
2021-02-15 01:25:51 +0900

    1. Order, Item entity에 비즈니스 로직 추가 2. NotEnoughStockException 추가 3. Item,Order service와 repository 추가 4. 주문 비즈니스로직 완성 5. OrderServiceTest 추가 6. OrderSearch와 find기능 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -15,16 +15,38 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Order order; //주문
 
-    private int orderPrice;
+    private int orderPrice; //주문 가격
 
-    private int count;
+    private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
